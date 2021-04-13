e89785f5cd55bd52ecf3c285f255ae9781f8b99e
Owen Davies <owen@odavi.es>
2021-04-13 20:49:48 +0000

    Lint Config.scala

--- src/main/scala/es/odavi/mandyville/common/Config.scala
+++ src/main/scala/es/odavi/mandyville/common/Config.scala
@@ -4,6 +4,7 @@ import com.typesafe.config._
 
 /** Factory object for mandyville.common.config instances */
 object Config {
+
   /** Returns the current environment we're in
     *
     * If the MANDYVILLE_ENV environemnt variable is defined, use that.
