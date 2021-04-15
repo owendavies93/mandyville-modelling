120b056595d851ae6299bbcbf4d3149c4f96c2a5
Owen Davies <owen@odavi.es>
2021-04-15 06:33:23 +0000

    Add package documentation for common package

---
+++ src/main/scala/es/odavi/mandyville/common/package.scala
@@ -0,0 +1,10 @@
+package es.odavi.mandyville
+
+/** mandyville.common - provides common code used across the modelling
+  * codebase, including:
+  *
+  *   - Configuration file access
+  *   - Database access
+  *   - Database Entity class mappings
+  */
+package object common {}
