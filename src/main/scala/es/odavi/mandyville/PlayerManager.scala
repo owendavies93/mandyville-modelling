9ba571aefb1a9f598fe2154716fda7a9b9ba343a
Owen Davies <owen@odavi.es>
2021-04-19 07:00:10 +0100

    Add getRelevantFixtures to PlayerManager

--- src/main/scala/es/odavi/mandyville/PlayerManager.scala
+++ src/main/scala/es/odavi/mandyville/PlayerManager.scala
@@ -1,8 +1,10 @@
 package es.odavi.mandyville
 
+import com.github.nscala_time.time.Imports.{richReadableInstant, DateTime}
 import es.odavi.mandyville.common.entity.PositionCategory._
 import es.odavi.mandyville.common.entity.{
   FPLSeasonInfo,
+  Fixture,
   Player,
   PlayerFixture,
   PositionCategory
@@ -12,7 +14,7 @@ import es.odavi.mandyville.common.entity.{
   * player related tasks
   */
 trait PlayerDatabaseService {
-  def getFixturesForPlayer(player: Player): List[PlayerFixture]
+  def getAllFixturesForPlayer(player: Player): List[(PlayerFixture, Fixture)]
   def getSeasonInfoForPlayer(player: Player, context: Context): FPLSeasonInfo
 }
 
@@ -23,10 +25,12 @@ private class PlayerDatabaseImp extends PlayerDatabaseService {
   import es.odavi.mandyville.common.Database.ctx
   import es.odavi.mandyville.common.Database.ctx._
 
-  // TODO: Use context here
-  def getFixturesForPlayer(player: Player): List[PlayerFixture] =
+  def getAllFixturesForPlayer(player: Player): List[(PlayerFixture, Fixture)] =
     ctx.run(quote {
-      playersFixtures.filter(p => p.playerId == lift(player.id))
+      playersFixtures
+        .join(fixtures)
+        .on(_.fixtureId == _.id)
+        .filter { case (p, f) => p.playerId == lift(player.id) }
     })
 
   def getSeasonInfoForPlayer(
@@ -70,5 +74,30 @@ class PlayerManager(service: PlayerDatabaseService) {
     val info = service.getSeasonInfoForPlayer(player, context)
 
     positionMap(info.fplSeasonId)
+  }
+
+  /** Get the relevant set of fixtures for the player in the given
+    * context i.e. the set of fixtures which we are allowed to use
+    * in the prediction
+    *
+    * @param player the player we're interested in
+    * @param context the context to compare fixtures against
+    */
+  def getRelevantFixtures(
+    player: Player,
+    context: Context
+  ): List[(PlayerFixture, Fixture)] = {
+    val allFixtures = service.getAllFixturesForPlayer(player)
+    val deadline = context.gameweek.deadline.withTimeAtStartOfDay()
+    allFixtures
+      .filter({
+        case (_, fixture) => fixture.fixtureDate.isDefined
+      })
+      .filter({
+        case (_, fixture) =>
+          val date = fixture.fixtureDate.get
+          val fixtureDt: DateTime = date.toDateTimeAtStartOfDay
+          fixtureDt < deadline
+      })
   }
 }
--- src/test/scala/es/odavi/mandyville/PlayerManager.scala
+++ src/test/scala/es/odavi/mandyville/PlayerManager.scala
@@ -1,19 +1,29 @@
 package es.odavi.mandyville
 
 import es.odavi.mandyville.common.entity.PositionCategory.Goalkeeper
-import es.odavi.mandyville.common.entity.{FPLGameweek, FPLSeasonInfo, Player}
-import org.joda.time.DateTime
+import es.odavi.mandyville.common.entity.{
+  FPLGameweek,
+  FPLSeasonInfo,
+  Fixture,
+  Player,
+  PlayerFixture
+}
+import org.joda.time.LocalDate
 import org.mockito.MockitoSugar
 import org.scalatest.funsuite.AnyFunSuite
+
+import scala.util.Random
 
 class PlayerManagerSuite extends AnyFunSuite with MockitoSugar {
   val dbService = mock[PlayerDatabaseService]
   val season: Short = 2020
   val goalkeeperId: Short = 1
+  val playerId = 1
 
   val seasonInfo = FPLSeasonInfo(1, 1, season, 1, goalkeeperId)
-  val player = Player(1, "Owen", "Davies", 1, None, None, None)
-  val gameweek = FPLGameweek(1, season, 1, DateTime.now())
+  val player = Player(playerId, "Owen", "Davies", 1, None, None, None)
+  val today = LocalDate.parse("2020-01-01")
+  val gameweek = FPLGameweek(1, season, 1, today.toDateTimeAtStartOfDay)
   val context = Context(gameweek)
 
   when(dbService.getSeasonInfoForPlayer(player, context)).thenReturn(seasonInfo)
@@ -22,5 +32,57 @@ class PlayerManagerSuite extends AnyFunSuite with MockitoSugar {
     assertResult(Goalkeeper) {
       new PlayerManager(dbService).getPositionForPlayer(player, context)
     }
+  }
+
+  val testFixtureDates =
+    List[Option[String]](Option("2020-01-02"), None, Option("2019-01-01"))
+
+  val fixtureInfo = testFixtureDates.map { dateString =>
+    val date =
+      if (dateString.isDefined) Option(LocalDate.parse(dateString.get))
+      else None
+    getDummyFixtureInfo(playerId, 10, date, season)
+  }
+
+  when(dbService.getAllFixturesForPlayer(player)).thenReturn(fixtureInfo)
+
+  test("Filters to relevant fixtures for the context") {
+    val relevantFixtures =
+      new PlayerManager(dbService).getRelevantFixtures(player, context)
+    assert(relevantFixtures.size == 1)
+  }
+
+  // Currently doesn't generate fixture stats as the tests don't
+  // require them
+  def getDummyFixtureInfo(
+    playerId: Int,
+    teamId: Int,
+    date: Option[LocalDate],
+    season: Short
+  ): (PlayerFixture, Fixture) = {
+    val playerFixtureId = Random.between(1, 10000)
+    val fixtureId = Random.between(1, 10000)
+    (
+      PlayerFixture(
+        playerFixtureId,
+        playerId,
+        fixtureId,
+        teamId,
+        90,
+        false,
+        false,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None
+      ),
+      Fixture(fixtureId, 1, teamId, 1, season, None, None, None, date)
+    )
   }
 }
