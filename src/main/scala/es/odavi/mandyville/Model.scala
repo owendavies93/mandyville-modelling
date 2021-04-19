55afa42de78eef9102821ab52fbacae6c613f960
Owen Davies <owen@odavi.es>
2021-04-19 07:06:53 +0100

    Add initial Model abstract class

---
+++ src/main/scala/es/odavi/mandyville/Model.scala
@@ -0,0 +1,89 @@
+package es.odavi.mandyville
+
+import common.entity.Player
+
+import scala.math.BigDecimal.RoundingMode
+
+/** The base predictive model, providing shared model functionality
+  * and an interface for all models.
+  *
+  * @constructor create a new model for a player in a context
+  * @param player the player we want to model
+  * @param context the context in which we are modelling
+  */
+abstract class Model(player: Player, context: Context) {
+
+  /** Find the predicted points for the gameweek provided in the
+    * given context.
+    *
+    * Fetch the expected values and probabilities of various in-game
+    * events, and combine them with the FPL ruleset to generate a
+    * points total.
+    *
+    * @return the total predicted points
+    */
+  def pointsForGameweek(playerManager: PlayerManager): BigDecimal = {
+    val mins = expectedMinutes()
+
+    if (mins == 0) 0
+    else {
+      val position = playerManager.getPositionForPlayer(player, context)
+
+      val goalPoints = expectedGoals() * GoalPoints(position)
+      val assistPoints = expectedAssists() * AssistPoints
+
+      val conceded = expectedConceded()
+
+      // TODO: do we need to think about whether the player is actually on the
+      //       pitch for the conceded goals here?
+      val cleanSheetPoints =
+        if (conceded < 0.5 && mins > 60) BigDecimal(CleanSheetPoints(position))
+        else if (conceded >= 1.5 && mins > 60) {
+          val lastEven = (conceded / 2).setScale(0, RoundingMode.FLOOR)
+          lastEven * TwoGoalsConcededPoints
+        } else BigDecimal(0)
+
+      val redPenalty = chanceOfRedCard() * RedCardPoints
+      val yellowPenalty = chanceOfYellowCard() * YellowCardPoints
+
+      val appearancePoints = if (mins >= 60) FullPlayPoints else SubPlayPoints
+
+      // TODO:
+      // * Penalty Saves
+      // * Penalty Misses
+      // * Own Goals
+      // * Bonus Points
+
+      goalPoints + assistPoints + cleanSheetPoints + redPenalty +
+        yellowPenalty + appearancePoints
+    }
+  }
+
+  /** Find the probability of the player getting a red card in the
+    * gameweek given by the context.
+    */
+  def chanceOfRedCard(): BigDecimal
+
+  /** Find the probability of the player getting a yellow card in the
+    * gameweek given by the context.
+    */
+  def chanceOfYellowCard(): BigDecimal
+
+  /** Find the expected number of assists for the player
+    */
+  def expectedAssists(): BigDecimal
+
+  /** Find the expected number of goals conceded by the player's team
+    * in the gameweek given by the context.
+    */
+  def expectedConceded(): BigDecimal
+
+  /** Find the expected number of goals scored by the player
+    */
+  def expectedGoals(): BigDecimal
+
+  /** Find the expected number of minutes played by the player in the
+    * gameweek given by the context
+    */
+  def expectedMinutes(): BigDecimal
+}
---
+++ src/test/scala/es/odavi/mandyville/Model.scala
@@ -0,0 +1,87 @@
+package es.odavi.mandyville
+
+import com.github.nscala_time.time.Imports.DateTime
+import es.odavi.mandyville.common.entity.PositionCategory.Goalkeeper
+import es.odavi.mandyville.common.entity.{FPLGameweek, FPLSeasonInfo, Player}
+import org.mockito.MockitoSugar
+import org.scalatest.funsuite.AnyFunSuite
+
+class ModelSuite extends AnyFunSuite with MockitoSugar {
+  val player = Player(1, "Owen", "Davies", 1, None, None, None)
+  val gameweek = FPLGameweek(1, 2020, 1, DateTime.now())
+  val context = Context(gameweek)
+  val dbService = mock[PlayerDatabaseService]
+  val seasonInfo = FPLSeasonInfo(1, 1, 2020, 1, 1)
+
+  when(dbService.getSeasonInfoForPlayer(player, context)).thenReturn(seasonInfo)
+
+  val playerManager = new PlayerManager(dbService)
+  val assists: BigDecimal = 1.0
+  val goals: BigDecimal = 1.0
+
+  class NoPlayModelStub extends Model(player, context) {
+    override def chanceOfRedCard() = 0
+    override def chanceOfYellowCard() = 0.4
+    override def expectedAssists(): BigDecimal = assists
+    override def expectedConceded() = 3.0
+    override def expectedGoals(): BigDecimal = goals
+    override def expectedMinutes() = 0
+  }
+
+  test("Returns zero points for zero minutes") {
+    assertResult(0) {
+      val noPlay = new NoPlayModelStub()
+      noPlay.pointsForGameweek(playerManager)
+    }
+  }
+
+  class Full90ModelStub extends NoPlayModelStub {
+    override def expectedMinutes() = 90
+  }
+
+  test("Returns correct points for full 90 minutes prediction") {
+    // -1 for conceded goals
+    // 2 for appearance
+    // -0.4 for yellow card penalty
+    val expectedPoints =
+      assists * AssistPoints + goals * GoalPoints(Goalkeeper) - 1 - 0.4 + 2
+
+    assertResult(expectedPoints) {
+      val full90 = new Full90ModelStub()
+      full90.pointsForGameweek(playerManager)
+    }
+  }
+
+  class Full90CleanSheetModel extends Full90ModelStub {
+    override def expectedConceded() = 0.4
+  }
+
+  test("Returns correct points for clean sheet") {
+    // 2 for appearance
+    // -0.4 for yellow card penalty
+    val expectedPoints =
+      assists * AssistPoints + goals * GoalPoints(Goalkeeper) +
+        CleanSheetPoints(Goalkeeper) - 0.4 + 2
+
+    assertResult(expectedPoints) {
+      val cleanSheet = new Full90CleanSheetModel()
+      cleanSheet.pointsForGameweek(playerManager)
+    }
+  }
+
+  class SubModel extends NoPlayModelStub {
+    override def expectedMinutes() = 15
+  }
+
+  test("Returns correct points total for substitute") {
+    // 1 for appearance
+    // -0.4 for yellow card penalty
+    val expectedPoints =
+      assists * AssistPoints + goals * GoalPoints(Goalkeeper) - 0.4 + 1
+
+    assertResult(expectedPoints) {
+      val sub = new SubModel()
+      sub.pointsForGameweek(playerManager)
+    }
+  }
+}
